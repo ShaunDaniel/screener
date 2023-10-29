f818c5e7c17562ade597f90f4b4c1f12e4ac7e63
s h a u n <73394707+ShaunDaniel@users.noreply.github.com>
2023-10-29 18:08:11 +0530

    added '/movie/:movieid' route
    this route will provide all info about a movie given its id

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ import express from "express"
 import axios from "axios"
 import cors from "cors"
 import dotenv from "dotenv"
+import morgan from "morgan"
 
 const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
 dotenv.config()
+
+app.use(morgan("combined"))
 
 app.use("/genres",(req,res)=>{
 
@@ -44,6 +47,28 @@ app.use("/explore/:genreid",(req,res)=>{
     .request(options)
     .then(function (response) {
         res.send(response.data);
+    })
+    .catch(function (error) {
+        console.error(error);
+    });
+})
+
+app.use("/movie/:movieid",(req,res)=>{
+    const movieId = req.params.movieid
+    const options = {
+    method: 'GET',
+    url: `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
+    headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${process.env.API_KEY}`
+    }
+    };
+
+    axios
+    .request(options)
+    .then(function (response) {
+        res.send(response.data);
+        console.log(response.data)
     })
     .catch(function (error) {
         console.error(error);
